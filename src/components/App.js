05cec5e589bf35dd7f425e282032fab9642a2160
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-26 16:24:49 -0400

    commit tree b1d5fec2acb68e582fe338dc7a0f4dcb25a1f2c3

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
+import About from './About'
+import Home from './Home'
+import NavBar from './NavBar'
 
 function App() {
   return (
     <div>
       <NavBar />
       <Home />
-      <About />
+      <About/>
     </div>
   );
 }
